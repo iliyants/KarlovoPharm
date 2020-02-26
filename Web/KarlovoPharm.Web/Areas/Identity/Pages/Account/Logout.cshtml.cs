089bc3612e8296b785e56be9a2cfbc656e54336d
iliyants <iliyan.lts@gmail.com>
2020-02-26 23:01:29 +0200

    Implemented panel for logged in users

---
+++ Web/KarlovoPharm.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,32 @@
+﻿namespace KarlovoPharm.Web
+{
+    using System.Threading.Tasks;
+
+    using KarlovoPharm.Data.Models.Common;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
+
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly ILogger<LogoutModel> logger;
+
+        public LogoutModel(SignInManager<ApplicationUser> signInManager, ILogger<LogoutModel> logger)
+        {
+            this.signInManager = signInManager;
+            this.logger = logger;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            await this.signInManager.SignOutAsync();
+
+            this.logger.LogInformation("User logged out.");
+
+            return this.Redirect("/");
+        }
+    }
+}
