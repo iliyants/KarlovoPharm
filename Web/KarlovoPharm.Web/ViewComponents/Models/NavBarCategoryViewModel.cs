69821d0664ffa0585818ff971442b1ff5988ea1c
iliyants <iliyan.lts@gmail.com>
2020-03-22 21:33:26 +0200

    Categories and their subCategories in the navbar are now invoked via view component.

---
+++ Web/KarlovoPharm.Web/ViewComponents/Models/NavBarCategoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace KarlovoPharm.Web.ViewComponents.Models
+{
+    using System.Collections.Generic;
+
+    using KarlovoPharm.Data.Models;
+    using KarlovoPharm.Services.Mapping;
+    using KarlovoPharm.Web.ViewModels.Categories;
+
+    public class NavBarCategoryViewModel : IMapFrom<Category>
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public IEnumerable<SubCategoryViewModel> SubCategories { get; set; }
+    }
+}
---
+++ Web/KarlovoPharm.Web/ViewComponents/NavBarCategoryComponent.cs
@@ -0,0 +1,25 @@
+﻿namespace KarlovoPharm.Web.ViewComponents
+{
+    using System.Threading.Tasks;
+
+    using KarlovoPharm.Services.Data.Categories;
+    using KarlovoPharm.Web.ViewComponents.Models;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class NavBarCategoryComponent : ViewComponent
+    {
+        private readonly ICategoryService categoryService;
+
+        public NavBarCategoryComponent(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await this.categoryService.GetAllAsync<NavBarCategoryViewModel>();
+
+            return this.View(categories);
+        }
+    }
+}
