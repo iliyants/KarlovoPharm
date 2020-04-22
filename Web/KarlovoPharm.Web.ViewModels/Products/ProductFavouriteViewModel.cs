84064f7c8fb30e42f39909bd26f490471af57434
iliyants <iliyan.lts@gmail.com>
2020-04-23 01:12:44 +0300

    User AddTocart button in Favourites disabled when product is unavailable.

--- Web/KarlovoPharm.Web.ViewModels/Products/ProductFavouriteViewModel.cs
+++ Web/KarlovoPharm.Web.ViewModels/Products/ProductFavouriteViewModel.cs
@@ -9,6 +9,8 @@
 
         public string ProductId { get; set; }
 
+        public bool ProductAvailable { get; set; }
+
         public string ProductName { get; set; }
 
         public decimal ProductPrice { get; set; }
